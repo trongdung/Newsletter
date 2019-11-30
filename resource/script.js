c46215f54c4920652d1333a339c4d1cb187a3e14
trongdung <dungtrong@gmail.com>
2019-11-30 17:38:34 +0100

    Test
    Test

---
+++ resource/script.js
@@ -0,0 +1,30 @@
+
+/* activate sidebar */
+$('#sidebar').affix({
+  offset: {
+    top: 405
+  }
+});
+
+/* activate scrollspy menu */
+var $body   = $(document.body);
+var navHeight = $('.navbar').outerHeight(true) + 10;
+
+$body.scrollspy({
+	target: '#leftCol',
+	offset: navHeight
+});
+
+/* smooth scrolling sections */
+$('a[href*=#]:not([href=#myCarousel])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 25
+        }, 1000);
+        return false;
+      }
+    }
+});
